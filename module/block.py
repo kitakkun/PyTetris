73b4376460027ef4872b1c136c4467861d00a83c
kitakkun <takumikitagawa24279@gmail.com>
2019-11-21 17:20:12 +0900

    Added block generation code
    Added block generation code.

---
+++ module/block.py
@@ -0,0 +1,99 @@
+import random, copy
+
+class Block:
+
+    EMPTY_BLOCK = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+
+    SQUARE_BLOCK = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0],
+        [0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+
+    BAR_BLOCK = [
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+
+    L_BLOCK = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0]
+    ]
+
+    L_INVERT_BLOCK = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+
+    Z_BLOCK = [
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+
+    Z_INVERT_BLOCK = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0],
+        [0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+
+    UNEVEN_BLOCK = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0]
+    ]
+
+
+    def __init__(self):
+        self.block = copy.deepcopy(self.EMPTY_BLOCK)
+        self.make()
+
+    # make a new block
+    def make(self):
+        self.clear()
+        type = random.randint(0, 6)
+        if type is 0:
+            self.block = copy.deepcopy(self.SQUARE_BLOCK)
+        elif type is 1:
+            self.block = copy.deepcopy(self.BAR_BLOCK)
+        elif type is 2:
+            self.block = copy.deepcopy(self.L_BLOCK)
+        elif type is 3:
+            self.block = copy.deepcopy(self.L_INVERT_BLOCK)
+        elif type is 4:
+            self.block = copy.deepcopy(self.Z_BLOCK)
+        elif type is 5:
+            self.block = copy.deepcopy(self.Z_INVERT_BLOCK)
+        elif type is 6:
+            self.block = copy.deepcopy(self.UNEVEN_BLOCK)
+        print(self.block)
+
+    def rotate(self):
+        pass
+
+    # reset block
+    def clear(self):
+        self.block = copy.deepcopy(self.EMPTY_BLOCK)
--- tetris.py
+++ tetris.py
@@ -1,8 +1,22 @@
 from module.board import Board
+from module.block import Block
+
+class Tetris:
+    def __init__(self):
+        self.board = Board()
+        self.block = Block()
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
 
 def main():
-    board = Board()
-    print(board.board)
+    tetris = Tetris()
+    tetris.start()
+    pass
 
 if __name__ == '__main__':
     main()
